ded124d1f999a5c62a478811eaef32da390504d7
Augustine Tambe <56054621+RepTambe@users.noreply.github.com>
2025-02-22 20:38:52 -0700

    Quartz sync: Feb 22, 2025, 8:38 PM

--- quartz.config.ts
+++ quartz.config.ts
@@ -9,7 +9,7 @@ import * as Plugin from "./quartz/plugins"
 const config: QuartzConfig = {
   configuration: {
     pageTitle: "🪴 My World",
-    pageTitleSuffix: "",
+    pageTitleSuffix: "World",
     enableSPA: true,
     enablePopovers: true,
     analytics: {
